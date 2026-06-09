063f30c1c1a0a6861407e3854e21fff55bc7aaf3
오승환 <osh8242@douzone.com>
2026-06-09 11:18:47 +0900

    feat(build): initialize Gradle wrapper and build configurations

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("java")
+    kotlin("jvm") version "1.9.22"
+    id("org.jetbrains.intellij.platform") version "2.1.0"
+}
+
+group = "com.github.plugin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    // CSV writer
+    implementation("org.apache.commons:commons-csv:1.10.0")
+    
+    // Apache POI for CSV to Excel conversion
+    implementation("org.apache.poi:poi:5.2.5")
+    implementation("org.apache.poi:poi-ooxml:5.2.5")
+    
+    intellijPlatform {
+        // Target DataGrip
+        datagrip("2023.3")
+        
+        // Target Database Tools plugin
+        bundledPlugin("com.intellij.database")
+        
+        instrumentationTools()
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        name.set("DataGrip Dataset Downloader")
+        id.set("com.github.plugin.datadownload")
+        version.set(project.version.toString())
+        
+        ideaVersion {
+            sinceBuild.set("233")
+            untilBuild.set("243.*")
+        }
+    }
+}
